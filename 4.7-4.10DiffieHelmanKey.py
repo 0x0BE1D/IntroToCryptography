5d69124021f905b1f0f31e417d10a77ffcf2488c
obeid2 <obeid2@gmail.com>
2023-01-08 12:06:46 +1100

    Create 4.7-4.10DiffieHelmanKey.py

---
+++ 4.7-4.10DiffieHelmanKey.py
@@ -0,0 +1,16 @@
+def calculate_key(p,alpha,x,y):
+    bigY=pow(alpha,y,p)
+    key=pow(bigY,x,p)
+    print(key)
+    
+calculate_key(877,2,25,3)
+calculate_key(907,2,32,153)
+calculate_key(1193,3,69,96)
+calculate_key(1471,6,51,22)
+
+'''
+794
+121
+489
+1084
+'''
